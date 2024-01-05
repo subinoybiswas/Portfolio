16e05ed3b6f04fa0bb2bee4b6a808a53a99af540
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-05 22:57:25 +0530

    Added spotlight, project from ProjectCards

--- src/app/spotlight.js
+++ src/app/spotlight.js
@@ -1,21 +1,16 @@
 import React, { useRef, useState, useEffect } from "react";
 import MousePosition from "./mouse-position";
 
-export default function Spotlight({
-  children,
-  className = "",
-}) {
+export default function Spotlight({ children, className = "" }) {
   const containerRef = useRef(null);
   const mousePosition = MousePosition();
   const mouse = useRef({ x: 0, y: 0 });
-  const containerSize = useRef({ w: 0, h: 0});
+  const containerSize = useRef({ w: 0, h: 0 });
   const [boxes, setBoxes] = useState([]);
 
   useEffect(() => {
     containerRef.current &&
-      setBoxes(
-        Array.from(containerRef.current.children).map((el) => el)
-      );
+      setBoxes(Array.from(containerRef.current.children).map((el) => el));
   }, []);
 
   useEffect(() => {
@@ -67,13 +62,10 @@ export default function Spotlight({
   );
 }
 
-export function SpotlightCard({
-  children,
-  className = "",
-}) {
+export function SpotlightCard({ children, className = "" }) {
   return (
     <div
-      className={`relative min-h-full w-[100vw] md:w-[30vw] bg-slate-800 rounded-3xl p-px before:absolute before:w-80 before:h-80 before:-left-40 before:-top-40 before:bg-slate-400 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:group-hover:opacity-100 before:z-10 before:blur-[100px] after:absolute after:w-96 after:h-96 after:-left-48 after:-top-48 after:bg-indigo-500 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-opacity after:duration-500 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:hover:opacity-10 after:z-30 after:blur-[100px] overflow-hidden ${className}`}
+      className={`relative min-h-full w-[100%] md:w-[30vw] bg-slate-800 rounded-3xl p-px before:absolute before:w-80 before:h-80 before:-left-40 before:-top-40 before:bg-slate-400 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-opacity before:duration-500 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:group-hover:opacity-100 before:z-10 before:blur-[100px] after:absolute after:w-96 after:h-96 after:-left-48 after:-top-48 after:bg-indigo-500 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-opacity after:duration-500 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:hover:opacity-10 after:z-30 after:blur-[100px] overflow-hidden ${className}`}
     >
       {children}
     </div>
