1663412c35b9392c075464ac655569bddf6f8fd6
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-05 21:52:48 +0530

    Cards Somewhat Works

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ import Image from "next/image";
 import Link from "next/link";
 import InstaLogo from "../../public/instagram-logo.png";
 import LinkedInLogo from "../../public/linkedin (1).png";
-import Spotlight from "./spotlight";
-import { SpotlightCard } from "./spotlight";
+import Project from "./project";
+
 export default function Home() {
   const [init, setInit] = useState(false);
   const particlesLoaded = (container) => {
@@ -22,14 +22,14 @@ export default function Home() {
     });
   }, []);
   return (
-    <main className=" select-none">
+    <main className=" select-none ">
       <div
         className="bg-fixed bg-gradient-to-bl h-[100vh] 
         from-[#000103] via-zinc-600/20 to-[#010610] flex flex-col  justify-center items-center p-24 text-center sm:w-auto w-screen"
       >
         {init && (
           <Particles
-            className="z-[-1]"
+            className="z-[-1] "
             id="tsparticles"
             particlesLoaded={particlesLoaded}
             options={{
@@ -140,53 +140,8 @@ bg-[length:200%_auto] animate-gradient pb-1 text-7xl sm:text-8xl "
           </Link>
         </div>
       </div>
-      <div className=" rounded-3xl  flex flex-col">
-        <Spotlight className="max-w-sm  mx-auto grid gap-6 lg:grid-cols-3 items-center lg:max-w-none group">
-          <SpotlightCard>
-            <div className="relative h-full bg-[#1d1543] p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
-              {/* Radial gradient */}
-              <div
-                className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
-                aria-hidden="true"
-              >
-                <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
-              </div>
-              <div className="flex flex-col h-full items-center text-center">
-                {/* Image */}
-                <div className="relative inline-flex">
-                  <div
-                    className="w-[40%] h-[40%] absolute inset-0 m-auto -translate-y-[10%] blur-3xl -z-10 rounded-full bg-white"
-                    aria-hidden="true"
-                  ></div>
-                </div>
-                {/* Text */}
-                <div className="grow mb-5">
-                  <h2 className="text-xl text-slate-200 font-bold mb-1">
-                    Amazing Integration
-                  </h2>
-                  <p className="text-sm text-slate-500">
-                    Quickly apply filters to refine your issues lists and create
-                    custom views.
-                  </p>
-                </div>
-                <a
-                  className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
-                  href="#0"
-                >
-                  <svg
-                    className="fill-slate-500 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="14"
-                  >
-                    <path d="M12.82 8.116A.5.5 0 0 0 12 8.5V10h-.185a3 3 0 0 1-2.258-1.025l-.4-.457-1.328 1.519.223.255A5 5 0 0 0 11.815 12H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM12.82.116A.5.5 0 0 0 12 .5V2h-.185a5 5 0 0 0-3.763 1.708L3.443 8.975A3 3 0 0 1 1.185 10H1a1 1 0 1 0 0 2h.185a5 5 0 0 0 3.763-1.708l4.609-5.267A3 3 0 0 1 11.815 4H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM1 4h.185a3 3 0 0 1 2.258 1.025l.4.457 1.328-1.52-.223-.254A5 5 0 0 0 1.185 2H1a1 1 0 0 0 0 2Z" />
-                  </svg>
-                  <span>Connect</span>
-                </a>
-              </div>
-            </div>
-          </SpotlightCard>
-        </Spotlight>
+      <div className="">
+        <Project></Project>
       </div>
     </main>
   );
