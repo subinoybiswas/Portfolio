c73604647bf8c21e2bea4e61609225a29d8524cd
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-06 00:06:34 +0530

    Test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import dynamic from "next/dynamic";
+const NoSSR = dynamic(() => import("./page.js"), { ssr: false });
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NoSSR></NoSSR>
+      </body>
     </html>
   );
 }
