5f7e071868ce08a5ea3e72a70812c288b8a8a642
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-09 00:20:05 +0530

    Added Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,6 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-6NQ6P5JBNG" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-6NQ6P5JBNG');
+        `}
+      </Script>
       <body className={inter.className}>
         {children}
         <Analytics />
