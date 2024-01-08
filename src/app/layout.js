8e1ed66b213095333ca09de4505dcc9569acbe82
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-08 23:33:38 +0530

    Fixed useEffectError

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
-import { initGA, logPageView } from "@/app/analytics";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,13 +8,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    initGA(); // Initialize Google Analytics
-    logPageView(); // Log the initial page view
-
-    // Add additional tracking logic as needed
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
