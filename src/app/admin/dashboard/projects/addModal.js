28953b3ab95ce6e4c724d09ffe5d96b8f09d5158
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-01-18 12:16:58 +0530

    refactor(admin/dashboard/projects): added modals for addProject and editProject

--- src/app/admin/dashboard/projects/addModal.js
+++ src/app/admin/dashboard/projects/addModal.js
@@ -1,21 +1,22 @@
-export default function EditModal(props) {
-  const current_project = props.current_project;
-  const handleChangeEdit = props.handleChangeEdit;
-  const editProject = props.editProject;
+export default function AddModal(props) {
+  const project = props.formData;
+  const handleChange = props.handleChange;
+  const addProject = props.addProject;
   const closeModal = props.closeModal;
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none ">
       <div className="bg-slate-50 md:h-[80vh] md:w-[90vw] flex md:flex-col rounded-xl">
         <div className="text-red-800 flex justify-end m-4">
           <button onClick={closeModal}>X</button>
         </div>
-        <div className="text-black flex justify-center text-3xl">Edit Project ID: {current_project.project_id}</div>
+        <div className="text-black flex justify-center text-3xl">
+          Add Project
+        </div>
         <div className="flex flex-col flex-grow justify-center ">
           <form
-            key={current_project.project_id}
             onSubmit={(e) => {
               e.preventDefault();
-              editProject(current_project.project_id);
+              addProject();
             }}
             className="mt-4 text-black flex flex-col justify-center gap-2 mx-4 "
           >
@@ -23,40 +24,36 @@ export default function EditModal(props) {
               type="text"
               name="title"
               placeholder="Title"
-              value={current_project.title}
               required
               className="border border-gray-300 rounded px-2 py-1 mt-2 "
-              onChange={handleChangeEdit}
+              onChange={handleChange}
             />
             <textarea
               name="content"
-              value={current_project.content}
               placeholder="Content"
               required
               className="border border-gray-300 rounded px-2 py-1 mt-2"
-              onChange={handleChangeEdit}
+              onChange={handleChange}
             ></textarea>
             <input
               type="text"
               name="githublink"
               placeholder="GitHub Link"
-              value={current_project.githublink}
               className="border border-gray-300 rounded px-2 py-1 mt-2"
-              onChange={handleChangeEdit}
+              onChange={handleChange}
             />
             <input
               type="text"
               name="weblink"
               placeholder="Web Link"
-              value={current_project.weblink}
               className="border border-gray-300 rounded px-2 py-1 mt-2"
-              onChange={handleChangeEdit}
+              onChange={handleChange}
             />
             <button
               type="submit"
               className="bg-green-500 text-white px-4 py-2 rounded mt-2 w-32"
             >
-              Edit Project
+              Add Project
             </button>
           </form>
         </div>
--- src/app/admin/dashboard/projects/page.js
+++ src/app/admin/dashboard/projects/page.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import EditModal from "./editModal";
+import AddModal from "./addModal";
 export default function Projects() {
   const [projects, setProjects] = useState([]);
   const [current_project, setCurrentProject] = useState({
@@ -16,7 +17,7 @@ export default function Projects() {
     githublink: "",
     weblink: "",
   });
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(0);
   useEffect(() => {
     fetchProjects();
   }, []);
@@ -42,6 +43,7 @@ export default function Projects() {
           "Content-Type": "application/json",
         },
       });
+      closeModal();
       fetchProjects();
     } catch (error) {
       console.error("Error adding project:", error);
@@ -65,7 +67,7 @@ export default function Projects() {
       const response = await axios.get(`/api/create-project/${projectId}`);
       setCurrentProject(response.data.projects[0]);
       console.log(response.data.projects);
-      setModal(true);
+      setModal(1);
     } catch (error) {
       console.error("Error editing project:", error);
     }
@@ -77,7 +79,7 @@ export default function Projects() {
         `/api/create-project/${projectId}`,
         current_project
       );
-      setModal(false);
+      closeModal();
       fetchProjects();
     } catch (error) {
       console.error("Error editing project:", error);
@@ -96,94 +98,94 @@ export default function Projects() {
       [e.target.name]: e.target.value,
     });
   };
+  const closeModal = () => {
+    setModal(0);
+  };
+  const addProjectModal = () => {
+    setModal(2);
+  };
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4 content-center">Projects Page</h1>
-      {/* Render projects list */}
-      {modal ? (
+    <div className="content-center justify-center items-center flex flex-col">
+      <h1 className="text-2xl font-bold mb-4">Projects Page</h1>
+
+      {modal === 1 ? (
         <EditModal
           current_project={current_project}
           handleChangeEdit={handleChangeEdit}
           editProject={editProject}
+          closeModal={closeModal}
+        />
+      ) : modal === 2 ? (
+        <AddModal
+          project={formData}
+          handleChange={handleChange}
+          addProject={addProject}
+          closeModal={closeModal}
         />
       ) : (
         <></>
       )}
 
-      {projects.map((project) => (
-        <div key={project.project_id} className="mb-4 flex flex-row gap-4">
-          <h2 className="text-xl font-bold">
-            {project.title},{project.project_id}
-          </h2>
-          <p>{project.content}</p>
-          <a
-            href={project.githublink}
-            className="text-blue-500 hover:underline"
-          >
-            GitHub Link
-          </a>
-          <a href={project.weblink} className="text-blue-500 hover:underline">
-            Web Link
-          </a>
-          <button
-            onClick={() => deleteProject(project.project_id)}
-            className="bg-red-500 text-white px-4 py-2 rounded"
-          >
-            Delete
-          </button>
-          <button
-            onClick={() => editProjectModal(project.project_id)}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
-          >
-            Edit
-          </button>
-        </div>
-      ))}
+      <table className="min-w-full bg-black border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 px-4 py-2">Title</th>
+            <th className="border border-gray-300 px-4 py-2">Content</th>
+            <th className="border border-gray-300 px-4 py-2">GitHub Link</th>
+            <th className="border border-gray-300 px-4 py-2">Web Link</th>
+            <th className="border border-gray-300 px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project.project_id}>
+              <td className="border border-gray-300 px-4 py-2">
+                {project.title}
+              </td>
+              <td className="border border-gray-300 px-4 py-2">
+                {project.content}
+              </td>
+              <td className="border border-gray-300 px-4 py-2">
+                <a
+                  href={project.githublink}
+                  className="text-blue-500 hover:underline"
+                >
+                  GitHub Link
+                </a>
+              </td>
+              <td className="border border-gray-300 px-4 py-2">
+                <a
+                  href={project.weblink}
+                  className="text-blue-500 hover:underline"
+                >
+                  Web Link
+                </a>
+              </td>
+              <td className="border border-gray-300 px-4 py-2">
+                <button
+                  onClick={() => deleteProject(project.project_id)}
+                  className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => editProjectModal(project.project_id)}
+                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-      {/* Add project form */}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          addProject(formData);
-        }}
-        className="mt-4 text-black"
+      <button
+        onClick={() => addProjectModal()}
+        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
       >
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          required
-          className="border border-gray-300 rounded px-2 py-1"
-          onChange={handleChange}
-        />
-        <textarea
-          name="content"
-          placeholder="Content"
-          required
-          className="border border-gray-300 rounded px-2 py-1 mt-2"
-          onChange={handleChange}
-        ></textarea>
-        <input
-          type="text"
-          name="githublink"
-          placeholder="GitHub Link"
-          className="border border-gray-300 rounded px-2 py-1 mt-2"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="weblink"
-          placeholder="Web Link"
-          className="border border-gray-300 rounded px-2 py-1 mt-2"
-          onChange={handleChange}
-        />
-        <button
-          type="submit"
-          className="bg-green-500 text-white px-4 py-2 rounded mt-2"
-        >
-          Add Project
-        </button>
-      </form>
+        Add Project
+      </button>
     </div>
   );
 }
