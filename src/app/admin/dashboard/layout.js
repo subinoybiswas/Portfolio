d9d0fc0aff407f3c38aa7662248269c071b6b939
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-04-22 14:17:31 +0530

    refactor: dashboard

--- src/app/admin/dashboard/layout.js
+++ src/app/admin/dashboard/layout.js
@@ -1,8 +1,35 @@
 "use client";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import {
+  Home,
+  LineChart,
+  Package,
+  Package2,
+  Settings,
+  ShoppingCart,
+  Users2,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 export default function AdminLayout({ children }) {
   const [isSuccess, setSuccess] = useState(false);
   const { push } = useRouter();
@@ -33,5 +60,134 @@ export default function AdminLayout({ children }) {
   if (!isSuccess) {
     return <p>Loading...</p>;
   }
-  return <main>{children}</main>;
+  const logoutBtn = async () => {
+    try {
+      await fetch("/api/logout");
+      push("/admin");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <TooltipProvider>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+          <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
+            <Link
+              href="#"
+              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            >
+              <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+              <span className="sr-only">Portifolio</span>
+            </Link>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <Home className="h-5 w-5" />
+                  <span className="sr-only">Dashboard</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Dashboard</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <ShoppingCart className="h-5 w-5" />
+                  <span className="sr-only">Orders</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Orders</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="dashboard/projects"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <Package className="h-5 w-5" />
+                  <span className="sr-only">Products</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Products</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <Users2 className="h-5 w-5" />
+                  <span className="sr-only">Customers</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Customers</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <LineChart className="h-5 w-5" />
+                  <span className="sr-only">Analytics</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Analytics</TooltipContent>
+            </Tooltip>
+          </nav>
+          <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-4">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <Settings className="h-5 w-5" />
+                  <span className="sr-only">Settings</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Settings</TooltipContent>
+            </Tooltip>
+          </nav>
+        </aside>
+        <div className="flex flex-col sm:gap-4 sm:py-0 sm:pl-14 justify-end">
+          <header className="flex items-center justify-end p-4 sm:px-6">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="overflow-hidden rounded-full"
+                >
+                  <Image
+                    src="/Me.jpg"
+                    width={36}
+                    height={36}
+                    alt="Avatar"
+                    className="overflow-hidden rounded-full"
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={logoutBtn}>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </header>
+          <Separator className="m-0 p-0" />
+          {children}
+        </div>
+      </div>
+    </TooltipProvider>
+  );
 }
--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,20 +1,9 @@
 "use client";
 import * as React from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-import {
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  Settings,
-  ShoppingCart,
-  Users2,
-} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
+
 import {
   Card,
   CardContent,
@@ -23,15 +12,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
 import {
   Table,
   TableBody,
@@ -44,14 +25,6 @@ import {
 
 import { useState, useEffect } from "react";
 import { Progress } from "@/components/ui/progress";
-import { Separator } from "@/components/ui/separator";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-  TooltipProvider,
-} from "@/components/ui/tooltip";
 
 export default function Dashboard() {
   const [data, setData] = useState("");
@@ -95,15 +68,7 @@ export default function Dashboard() {
   }, []);
 
   const { push } = useRouter();
-  const logoutBtn = async () => {
-    try {
-      await fetch("/api/logout");
-      push("/admin");
-    } catch (e) {
-      console.log(e);
-    }
-  };
- 
+
   const goal = {
     totalUsers: 2000,
     averageSessionDuration: 500,
@@ -114,200 +79,78 @@ export default function Dashboard() {
   };
 
   return (
-    <TooltipProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
-        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-          <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
-            <Link
-              href="#"
-              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-            >
-              <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-              <span className="sr-only">Portifolio</span>
-            </Link>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+    <main className="grid flex-1 items-start gap-4 p-4 lg:grid-cols-3 ">
+      <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+          {!loading &&
+            data.map((metric, index) => {
+              const goalValue = goal[metric.fieldName];
+              return (
+                <Card
+                  x-chunk="dashboard-05-chunk-1"
+                  className="min-h-[200px] flex flex-col gap-2"
+                  key={index}
                 >
-                  <Home className="h-5 w-5" />
-                  <span className="sr-only">Dashboard</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Dashboard</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <ShoppingCart className="h-5 w-5" />
-                  <span className="sr-only">Orders</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Orders</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="dashboard/projects"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Package className="h-5 w-5" />
-                  <span className="sr-only">Products</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Products</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Users2 className="h-5 w-5" />
-                  <span className="sr-only">Customers</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Customers</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <LineChart className="h-5 w-5" />
-                  <span className="sr-only">Analytics</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Analytics</TooltipContent>
-            </Tooltip>
-          </nav>
-          <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-4">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="#"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Settings className="h-5 w-5" />
-                  <span className="sr-only">Settings</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Settings</TooltipContent>
-            </Tooltip>
-          </nav>
-        </aside>
-        <div className="flex flex-col sm:gap-4 sm:py-0 sm:pl-14 justify-end">
-          <header className="flex items-center justify-end p-4 sm:px-6">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="overflow-hidden rounded-full"
-                >
-                  <Image
-                    src="/Me.jpg"
-                    width={36}
-                    height={36}
-                    alt="Avatar"
-                    className="overflow-hidden rounded-full"
-                  />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuItem>Support</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={logoutBtn}>Logout</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </header>
-          <Separator className="m-0 p-0" />
-          <main className="grid flex-1 items-start gap-4 p-4 lg:grid-cols-3 ">
-            <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-              <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-                {!loading &&
-                  data.map((metric, index) => {
-                    const goalValue = goal[metric.fieldName];
-                    return (
-                      <Card
-                        x-chunk="dashboard-05-chunk-1"
-                        className="min-h-[200px] flex flex-col gap-2"
-                        key={index}
-                      >
-                        <CardHeader className="pb-2 gap-2">
-                          <CardDescription>{metric.fieldName}</CardDescription>
-                          <CardTitle className="text-4xl">
-                            {Number(metric.value).toFixed(2)}
-                          </CardTitle>
-                        </CardHeader>
-                        <CardContent>{}</CardContent>
-                        <CardFooter>
-                          <Progress
-                            value={
-                              (Number(metric.value).toFixed(2) / goalValue) *
-                              100
-                            }
-                            aria-label="25% increase"
-                          />
-                        </CardFooter>
-                      </Card>
-                    );
-                  })}
-              </div>
-            </div>
-            {!loading ? (
-              <Table>
-                <TableCaption>A list of the commits </TableCaption>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[100px]">Commit</TableHead>
-                    <TableHead>Message</TableHead>
-                    <TableHead>User</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {commitData.map((commit, index) => (
-                    <>
-                      <TableRow>
-                        <TableCell
-                          className="font-medium cursor-pointer"
-                          onClick={() =>
-                            window.open(commit.commitUrl, "_blank")
-                          }
-                        >
-                          {commit.shortSha}
-                        </TableCell>
-                        <TableCell>{commit.commitMessage}</TableCell>
-                        <TableCell>
-                          <Avatar
-                            className="cursor-pointer"
-                            onClick={() =>
-                              window.open(commit.committerLink, "_blank")
-                            }
-                          >
-                            <AvatarImage src={commit.committerAvatar} />
-                            <AvatarFallback>CM</AvatarFallback>
-                          </Avatar>
-                        </TableCell>
-                      </TableRow>
-                    </>
-                  ))}
-                </TableBody>
-              </Table>
-            ) : (
-              <></>
-            )}
-          </main>
+                  <CardHeader className="pb-2 gap-2">
+                    <CardDescription>{metric.fieldName}</CardDescription>
+                    <CardTitle className="text-4xl">
+                      {Number(metric.value).toFixed(2)}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>{}</CardContent>
+                  <CardFooter>
+                    <Progress
+                      value={
+                        (Number(metric.value).toFixed(2) / goalValue) * 100
+                      }
+                      aria-label="25% increase"
+                    />
+                  </CardFooter>
+                </Card>
+              );
+            })}
         </div>
       </div>
-    </TooltipProvider>
+      {!loading ? (
+        <Table>
+          <TableCaption>A list of the commits </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Commit</TableHead>
+              <TableHead>Message</TableHead>
+              <TableHead>User</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {commitData.map((commit, index) => (
+              <>
+                <TableRow>
+                  <TableCell
+                    className="font-medium cursor-pointer"
+                    onClick={() => window.open(commit.commitUrl, "_blank")}
+                  >
+                    {commit.shortSha}
+                  </TableCell>
+                  <TableCell>{commit.commitMessage}</TableCell>
+                  <TableCell>
+                    <Avatar
+                      className="cursor-pointer"
+                      onClick={() =>
+                        window.open(commit.committerLink, "_blank")
+                      }
+                    >
+                      <AvatarImage src={commit.committerAvatar} />
+                      <AvatarFallback>CM</AvatarFallback>
+                    </Avatar>
+                  </TableCell>
+                </TableRow>
+              </>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <></>
+      )}
+    </main>
   );
 }
