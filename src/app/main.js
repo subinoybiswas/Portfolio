2ae3fdb9c1def741da7ca37995015347a3f3b747
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-04-28 12:39:59 +0530

    update: expreieinces

--- src/app/main.js
+++ src/app/main.js
@@ -23,6 +23,7 @@ import GridLines from "react-gridlines";
 import { SocialIcon } from "react-social-icons";
 import { useRouter } from "next/navigation";
 import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+import content from "@/helpers/experiences";
 export default function Main() {
   const { push } = useRouter();
   const [init, setInit] = useState(false);
@@ -73,78 +74,7 @@ export default function Main() {
     });
   };
   const [imgLoading, setImgLoading] = useState(true);
-  const content = [
-    {
-      title: "Collaborative Editing",
-      description:
-        "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
-      content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-          Collaborative Editing
-        </div>
-      ),
-    },
-    {
-      title: "Real time changes",
-      description:
-        "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-      content: (
-        <div className="h-full w-full  flex items-center justify-center text-white">
-          <Image
-            src="/linear.webp"
-            width={300}
-            height={300}
-            className="h-full w-full object-cover"
-            alt="linear board demo"
-          />
-        </div>
-      ),
-    },
-    {
-      title: "Version control",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-          Version control
-        </div>
-      ),
-    },
-    {
-      title: "Running out of content",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <Image
-          className={`${
-            imgLoading ? "animate-pulse bg-gray-200 blur-md" : ""
-          } mb-5 rounded-md`}
-          src={"/Me.jpg"}
-          style={{
-            width: "100%",
-            height: "auto",
-            objectFit: "cover",
-            aspectRatio: "3/2",
-          }}
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Image"
-          onLoadingComplete={() => setImgLoading(false)}
-        ></Image>
-      ),
-    },
-    {
-      title: "Running out of content",
-      description:
-        "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-      content: (
-        <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-          Running out of content
-        </div>
-      ),
-    },
-  ];
+  
   const tabs = [
     {
       title: "Product",
---
+++ src/helpers/experiences.js
@@ -0,0 +1,115 @@
+import { Image } from "@nextui-org/react";
+const content = [
+  {
+    title: "PlayTheory Labs",
+    description:
+      "At Playtheory Labs, I excelled in backend development using Express.js and JavaScript, optimizing web apps. Leveraging AWS, especially Lambda Functions, I spearheaded serverless architecture for scalability. Additionally, I automated deployment processes and crafted Chrome extensions, also utilizing tools like Clerk, Stripe, and MongoDB for innovative SaaS products.",
+    content: (
+      <Image
+        className={`mb-5 rounded-md`}
+        src={"/playtheory.png"}
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          aspectRatio: "3/2",
+        }}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Image"
+        onLoadingComplete={() => setImgLoading(false)}
+      ></Image>
+    ),
+  },
+  {
+    title: "DevDotCom",
+    description:
+      "As the Founder and Lead of DevDotCom, I spearheaded the growth of the community from inception to over 1000 members through strategic outreach and engagement initiatives. Organizing six coding events, I demonstrated proficient project management skills, fostering a collaborative environment. Additionally, I orchestrated Eastern India's largest offline Open Source event, championing collaboration and knowledge-sharing among technology enthusiasts.",
+    content: (
+      <Image
+        className={`mb-5 rounded-md`}
+        src={"/oss.jpg"}
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          aspectRatio: "3/2",
+        }}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Image"
+        onLoadingComplete={() => setImgLoading(false)}
+      ></Image>
+    ),
+  },
+  {
+    title: "JISCE Coding Club",
+    description:
+      "As a core team member of JISCE Coding Club, I played a pivotal role in organizing multiple coding club events. Collaborating closely with fellow team members, I contributed to the seamless execution of engaging and informative events aimed at fostering a passion for coding among students. Our efforts facilitated a vibrant community where members could learn, share knowledge, and collaborate on coding projects.",
+    content: (
+      <Image
+        className={`mb-5 rounded-md`}
+        src={"/CodingClub.jpg"}
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          aspectRatio: "3/2",
+        }}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Image"
+        onLoadingComplete={() => setImgLoading(false)}
+      ></Image>
+    ),
+  },
+  {
+    title: "Status Code 0",
+    description:
+      "Winning StatusCode 0, a 36-hour MLH hackathon hosted by IIIT Kalyani and IISER Kolkata, was incredibly rewarding. My team's collaboration, creativity, and perseverance led us to victory, showcasing our ability to innovate under pressure. It was a memorable experience filled with learning and camaraderie.",
+    content: (
+      <Image
+        className={`mb-5 rounded-md`}
+        src={"/sc0.jpg"}
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          aspectRatio: "3/2",
+        }}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Image"
+        onLoadingComplete={() => setImgLoading(false)}
+      ></Image>
+    ),
+  },
+  {
+    title: "GDG Kolkata",
+    description:
+      "As a core team member of GDG Kolkata, I've been part of facilitating large-scale events like Devfest 2k23 Kolkata and International Women's Day celebrations. At GDG Kolkata, our focus is on fostering a vibrant community where developers, entrepreneurs, professionals, and students come together to learn, teach, and form lasting connections. It's fulfilling to contribute to an inclusive environment that promotes knowledge sharing and collaboration among tech enthusiasts.",
+    content: (
+      <Image
+        className={`mb-5 rounded-md`}
+        src={"/gdgkol.jpg"}
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          aspectRatio: "3/2",
+        }}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Image"
+        onLoadingComplete={() => setImgLoading(false)}
+      ></Image>
+    ),
+  },
+];
+
+export default content;
